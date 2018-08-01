be28d137b2b78a1819a0748ff926521d09b7601a
wgx731 <wgx731@gmail.com>
2018-08-01 18:58:16 +0700

    [DEV]: #2
    1. remove site part
    2. better exit code

--- scripts/build-artifacts.sh
+++ scripts/build-artifacts.sh
@@ -4,14 +4,16 @@ echo "[BUILD-ARTIFACTS] source config.sh ..."
 source $PWD/scripts/config.sh || exit 20
 
 echo "[BUILD-ARTIFACTS] building artifacts ..."
-$PWD/mvnw clean package site || exit 21
+$PWD/mvnw clean package || exit 21
 
 echo "[BUILD-ARTIFACTS] packaging artifacts ..."
 cp $PWD/api-gateway/target/*.jar \
   $PWD/target || exit 22
 for module in ${MODULES}
 do
-    mkdir -p $PWD/target/site/${module} || exit 22
-    cp -R $PWD/${module}/target/site/* \
+    if [ -d "$PWD/${module}/target/site" ]; then
+      mkdir -p $PWD/target/site/${module} || exit 22
+      cp -R $PWD/${module}/target/site/* \
         $PWD/target/site/${module} || exit 22
+     fi
 done
