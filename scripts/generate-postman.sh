0daca1b68a599840e563c2d0bf273a48099f994d
wgx731 <wgx731@gmail.com>
2018-08-01 19:13:16 +0700

    [BUG]: #2
    create directory outside docker

--- scripts/generate-postman.sh
+++ scripts/generate-postman.sh
@@ -3,15 +3,12 @@
 echo "[DOCS - POSTMAN] source config.sh ..."
 source $PWD/scripts/config.sh || exit 30
 
-echo "[DOCS - POSTMAN] create postman directory ..."
-mkdir -p $PWD/api-gateway/target/docs/postman || exit 31
-
 echo "[DOCS - POSTMAN] generate postman collection json ..."
 restdocs-to-postman \
   -i $PWD/api-gateway/target/docs/rest/snippets \
   -e postman \
   -f secondLastFolder \
-  -o $PWD/api-gateway/target/docs/postman/collection.json || exit 32
+  -o $PWD/api-gateway/target/docs/postman/collection.json || exit 31
 
 echo "[DOCS - POSTMAN] show postman collection:"
-cat $PWD/api-gateway/target/docs/postman/collection.json || exit 33
+cat $PWD/api-gateway/target/docs/postman/collection.json || exit 32
