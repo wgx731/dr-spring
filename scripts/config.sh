f8a07d1f598edbff6b2cbbc8f7f833d6cea2fa78
wgx731 <wgx731@gmail.com>
2018-08-02 12:34:16 +0700

    [RELEASE]: fix sed problem

--- scripts/config.sh
+++ scripts/config.sh
@@ -4,3 +4,4 @@ export RELEASE_VERSION="1.3.0"
 export SNAPSHOT_VERSION="1.3.1-SNAPSHOT"
 export TARGET_URL="http://$DEV_IP:8080"
 export MODULES="common api-gateway"
+export OS=`uname`
--- scripts/make-release.sh
+++ scripts/make-release.sh
@@ -2,7 +2,11 @@
 
 function setAppVer {
     for i in `find . -name "application.properties"`; do
-        sed -i "" "s/^info.app.version=.*/info.app.version=$1/g" ${i}
+        if [[ "$OS" == 'Darwin' ]]; then
+            sed -i "" "s/^info.app.version=.*/info.app.version=$1/g" ${i}
+        elfi
+            sed -i "s/^info.app.version=.*/info.app.version=$1/g" ${i}
+        fi
         grep -q "^info.app.version=.*" ${i} || echo "info.app.version=$1" >> ${i}
     done
 }
@@ -15,7 +19,11 @@ git checkout master || exit 61
 git pull || exit 61
 git merge origin/dev || exit 61
 $PWD/mvnw versions:set -DnewVersion=${RELEASE_VERSION} || exit 61
-sed -i "s|<dr-spring.version>.*<\/dr-spring.version>|<dr-spring.version>$RELEASE_VERSION<\/dr-spring.version>|" pom.xml || exit 61
+if [[ "$OS" == 'Darwin' ]]; then
+  sed -i "" "s|<dr-spring.version>.*<\/dr-spring.version>|<dr-spring.version>$RELEASE_VERSION<\/dr-spring.version>|" pom.xml || exit 61
+elfi
+  sed -i "s|<dr-spring.version>.*<\/dr-spring.version>|<dr-spring.version>$RELEASE_VERSION<\/dr-spring.version>|" pom.xml || exit 61
+fi
 setAppVer ${RELEASE_VERSION} || exit 61
 git add pom.xml */pom.xml */application.properties || exit 61
 git commit -m "[RELEASE]: create release with version $RELEASE_VERSION in 'master' branch :octopus:" || exit 61
@@ -27,7 +35,11 @@ echo "[MAKE-RELEASE] creating snapshot with version $SNAPSHOT_VERSION in 'dev' b
 git checkout dev || exit 62
 git pull || exit 62
 $PWD/mvnw versions:set -DnewVersion=${SNAPSHOT_VERSION} || exit 62
-sed -i "s|<dr-spring.version>.*<\/dr-spring.version>|<dr-spring.version>$SNAPSHOT_VERSION<\/dr-spring.version>|" pom.xml || exit 62
+if [[ "$OS" == 'Darwin' ]]; then
+  sed -i "" "s|<dr-spring.version>.*<\/dr-spring.version>|<dr-spring.version>$SNAPSHOT_VERSION<\/dr-spring.version>|" pom.xml || exit 62
+elfi
+  sed -i "s|<dr-spring.version>.*<\/dr-spring.version>|<dr-spring.version>$SNAPSHOT_VERSION<\/dr-spring.version>|" pom.xml || exit 62
+fi
 setAppVer ${SNAPSHOT_VERSION} || exit 62
 git add pom.xml */pom.xml */application.properties || exit 62
 git commit -m "[RELEASE]: create snapshot with version $SNAPSHOT_VERSION in 'dev' branch :snake:" || exit 62
