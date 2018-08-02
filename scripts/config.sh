7106c48e23a0afe6d1c14786022ce2746c7cd158
wgx731 <wgx731@gmail.com>
2018-08-02 17:50:24 +0700

    [RELEASE]: close #2
    1.3.1 release

--- scripts/config.sh
+++ scripts/config.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-export RELEASE_VERSION="1.3.0"
-export SNAPSHOT_VERSION="1.3.1-SNAPSHOT"
+export RELEASE_VERSION="1.3.1"
+export SNAPSHOT_VERSION="1.3.2-SNAPSHOT"
 export TARGET_URL="http://$DEV_IP:8080"
 export MODULES="dr-spring-common dr-spring-api"
 export OS=`uname`
