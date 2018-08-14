05c5fd564a6a076806efa1c1350afcf5adeeaad2
wgx731 <wgx731@gmail.com>
2018-08-14 09:05:07 +0800

    [RELEASE]: close #7
    1.3.5 release

--- scripts/config.sh
+++ scripts/config.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-export RELEASE_VERSION="1.3.4"
-export SNAPSHOT_VERSION="1.3.5-SNAPSHOT"
+export RELEASE_VERSION="1.3.5"
+export SNAPSHOT_VERSION="1.3.6-SNAPSHOT"
 export TARGET_URL="http://$DEV_IP:8080"
 export DOCKER_MODULES="dr-spring-dubbo-provider dr-spring-grpc-provider dr-spring-gateway"
 export MODULES="dr-spring-common $DOCKER_MODULES"
