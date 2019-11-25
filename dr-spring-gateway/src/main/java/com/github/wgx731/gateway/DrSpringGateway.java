359c67355212d0d02dc3df1ff69b98d3b1bdaada
wgx731 <wgx731@gmail.com>
2019-11-25 23:48:07 +0800

    [DEV]: upgrade dr-spring

--- dr-spring-gateway/src/main/java/com/github/wgx731/gateway/DrSpringGateway.java
+++ dr-spring-gateway/src/main/java/com/github/wgx731/gateway/DrSpringGateway.java
@@ -1,13 +1,11 @@
 package com.github.wgx731.gateway;
 
-import akka.stream.alpakka.spring.web.SpringWebAkkaStreamsConfiguration;
 import com.github.wgx731.gateway.properties.GrpcProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
-// TODO: follow up on spring web akka
-@SpringBootApplication(exclude = SpringWebAkkaStreamsConfiguration.class)
+@SpringBootApplication
 @EnableConfigurationProperties(GrpcProperties.class)
 public class DrSpringGateway {
 
