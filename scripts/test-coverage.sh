a85250eee88be79aab3e1fd4db9d042a6486423e
wgx731 <wgx731@gmail.com>
2019-11-26 22:19:14 +0800

    [CICD]: #7 - add count source code lines

--- scripts/test-coverage.sh
+++ scripts/test-coverage.sh
@@ -3,8 +3,18 @@
 echo "[TEST-COVERAGE] source config.sh ..."
 source ${PWD}/scripts/config.sh || exit 20
 
+echo "[TEST-COVERAGE] count source code lines ..."
+echo "total lines: $(find . -name "*.java" | xargs cat | grep "[a-zA-Z0-9{}]" | wc -l | tr -d ' ')"
+for module in ${MODULES}
+do
+  echo "${module} source code lines: $(find ./${module}/src/main -name "*.java" \
+    | xargs cat | grep "[a-zA-Z0-9{}]" | wc -l | tr -d ' ')"
+  echo "${module} test code lines: $(find ./${module}/src/test -name "*.java" \
+    | xargs cat | grep "[a-zA-Z0-9{}]" | wc -l | tr -d ' ')"
+done
+
 echo "[TEST-COVERAGE] generate coverage report ..."
-${PWD}/mvnw clean verify || exit 21
+${PWD}/mvnw -q clean verify || exit 21
 
 echo "[TEST-COVERAGE] parse and show jacoco report:"
 for module in ${MODULES}
